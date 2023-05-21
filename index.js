e134080368a5c72ec3b5c62f05ab0ba84f0350a9
ebadulhassan <ebadulhassan967@gmail.com>
2023-05-21 14:38:05 -0700

    assigment3

---
+++ index.js
@@ -0,0 +1,24 @@
+// let num1 = prompt("enter first value");
+// let num2 = prompt("enter second value");
+
+// let result = num1/num2;
+// if(result==Infinity){
+//     document.write("maths error")
+// }
+// else{
+//     document.write(result )
+// }
+
+
+
+
+let user1 = prompt("Enter The First Number")
+let user2 = prompt("Enter The Second Number")
+let result = user1/user2
+if (result==Infinity){
+    document.write("maths errors")
+}
+else {
+    document.write(result)
+
+}
